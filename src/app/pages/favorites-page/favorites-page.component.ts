fc221e4716fcb18175fe3feef65167e04219e3e5
cooperfirewalk <cooperfirewalk@gmail.com>
2025-07-28 09:10:47 +0300

    добавляет changeDetection onpush

--- src/app/pages/favorites-page/favorites-page.component.ts
+++ src/app/pages/favorites-page/favorites-page.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-favorites-page',
   imports: [],
+  standalone: true,
   templateUrl: './favorites-page.component.html',
   styleUrl: './favorites-page.component.css'
 })
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { OfferComponent } from './offer/offer.component';
 
 @Component({
@@ -6,7 +6,8 @@ import { OfferComponent } from './offer/offer.component';
   imports: [OfferComponent],
   standalone: true,
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.css'
+  styleUrl: './main-page.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainPageComponent {
   @Input({required: true}) offersCount!: number
