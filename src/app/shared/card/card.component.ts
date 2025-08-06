fe36e7b2573acb4057f9859e8579996b049a8e12
cooperfirewalk <cooperfirewalk@gmail.com>
2025-08-06 10:46:04 +0300

    добавляет отрисовку офферов в card по параметрам из моков

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
+import { Offer } from '../../app.model';
+import { CommonModule } from '@angular/common';
+
+const STARS_STYLE_COEFF = 20;
 
 @Component({
   selector: 'app-card',
-  imports: [],
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
+  offer = input.required<Offer>()
+  starsStyle = computed(() => 'width:' + Math.round(this.offer().rating * STARS_STYLE_COEFF ) + '%')
 
 }
