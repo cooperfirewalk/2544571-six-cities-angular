3a59b4c4c780dd40f5cc0dd03892d28f1cfe8fc0
cooperfirewalk <cooperfirewalk@gmail.com>
2025-08-06 12:48:51 +0300

    добавляет отрисовку офферов в фэйворитс

--- src/app/pages/favorites-page/favorites-page.component.ts
+++ src/app/pages/favorites-page/favorites-page.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { OffersService } from '../offers.service';
+import { CardComponent } from '../../shared/card/card.component';
+import { CardClass } from '../../shared/card/card.model';
 
 @Component({
   selector: 'app-favorites-page',
-  imports: [],
+  imports: [CardComponent],
   standalone: true,
   templateUrl: './favorites-page.component.html',
   styleUrl: './favorites-page.component.css'
 })
 export class FavoritesPageComponent {
-
+  cardClass = CardClass
+  private offersService = inject(OffersService)
+  offers = this.offersService.offers
+  uniqueCities = computed(() => {
+    const array:string[] = []
+    this.offers.forEach((element) => {
+      if (element.city.name !== array[array.length - 1]) {
+        array.push(element.city.name);
+      }
+    }
+  )
+  return array
+  })
 }
