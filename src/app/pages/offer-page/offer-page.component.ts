97662e6caf26e5df0e08a12f97942a865e48ac8d
cooperfirewalk <cooperfirewalk@gmail.com>
2025-08-06 15:32:20 +0300

    добавляет шаблон формы

--- src/app/pages/offer-page/offer-page.component.ts
+++ src/app/pages/offer-page/offer-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ReviewFormComponent } from './review-form/review-form.component';
 
 @Component({
   selector: 'app-offer-page',
-  imports: [RouterLink],
+  imports: [RouterLink, ReviewFormComponent],
   standalone: true,
   templateUrl: './offer-page.component.html',
   styleUrl: './offer-page.component.css'
