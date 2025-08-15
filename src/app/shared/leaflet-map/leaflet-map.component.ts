2ff45829270dbd6b7b90a3a85b37c6b2e72e6177
cooperfirewalk <cooperfirewalk@gmail.com>
2025-08-15 12:19:43 +0300

    нарисовали несколько точек с помощью ngx-leaflet

--- src/app/shared/leaflet-map/leaflet-map.component.ts
+++ src/app/shared/leaflet-map/leaflet-map.component.ts
@@ -1,6 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { OffersService } from '../../pages/offers.service';
 import { LeafletModule } from '@bluehalo/ngx-leaflet';
+import { icon, IconOptions, Layer, marker } from 'leaflet';
 import { latLng, tileLayer } from 'leaflet';
+import { URL_MARKER_CURRENT, URL_MARKER_DEFAULT } from './leaflet-map.model';
+import { CITIES } from '../../app.model';
+
+const defaultCustomIcon: IconOptions = {
+  iconUrl: URL_MARKER_DEFAULT,
+  iconSize: [27, 39],
+  iconAnchor: [13.5, 39],
+};
+
+const currentCustomIcon: IconOptions = {
+  iconUrl: URL_MARKER_CURRENT,
+  iconSize: [27, 39],
+  iconAnchor: [13.5, 39],
+};
 @Component({
   selector: '[app-leaflet-map]',
   templateUrl: './leaflet-map.component.html',
@@ -9,12 +25,31 @@ import { latLng, tileLayer } from 'leaflet';
   imports: [LeafletModule]
 })
 export class LeafletMapComponent {
+  private offersService = inject(OffersService)
+  offers = this.offersService.offers
+
   options = {
     layers: [
-      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+      tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+        {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        })
     ],
-    zoom: 5,
-    center: latLng(46.879966, -121.726909)
+    zoom: CITIES[0].location.zoom,
+    center: latLng(CITIES[0].location.latitude, CITIES[0].location.longitude)
   };
 
+  markers: Layer[] = [
+    marker(
+      [this.offers[0].location.latitude, this.offers[0].location.longitude],
+      {
+        icon: icon(defaultCustomIcon)
+      }),
+    marker(
+      [this.offers[2].location.latitude, this.offers[2].location.longitude],
+      {
+        icon: icon(currentCustomIcon)
+      })
+  ]
 }
