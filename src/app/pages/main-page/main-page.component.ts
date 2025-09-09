6e30de4ee900159df4ca43332c60d2df777efe6e
cooperfirewalk <cooperfirewalk@gmail.com>
2025-09-09 22:15:39 +0300

    добавляет закрытие окна сортировки по клику и esc

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { OffersListComponent } from './offers-list/offers-list.component';
 import { RouterLink } from '@angular/router';
 import { LeafletMapComponent } from '../../shared/leaflet-map/leaflet-map.component';
@@ -16,6 +16,18 @@ import { CITIES } from '../../app.model';
 export class MainPageComponent {
   private offersService = inject(OffersService)
   citiesArray = CITIES
+  isSortingOpen = signal <boolean>(false)
+  sortingOpenToogle(needClose?: string) {
+    if(needClose) {
+      this.isSortingOpen.set(false)
+      return
+    }
+    if(this.isSortingOpen()) {
+      this.isSortingOpen.set(false)
+      return
+    }
+    this.isSortingOpen.set(true)
+  }
 
   onTabClick (cityName: string) {
     this.offersService.changeSelectedCity(cityName)
