432c6a48a2f84d2f2f0624fae88ff66bbb3e6516
cooperfirewalk <cooperfirewalk@gmail.com>
2025-09-10 16:23:11 +0300

    добавляет карту в offer page , избавляется от обертки на карте

--- src/app/pages/offer-page/offer-page.component.ts
+++ src/app/pages/offer-page/offer-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ReviewFormComponent } from './review-form/review-form.component';
+import { LeafletMapComponent } from "../../shared/leaflet-map/leaflet-map.component";
 
 @Component({
   selector: 'app-offer-page',
-  imports: [RouterLink, ReviewFormComponent],
+  imports: [RouterLink, ReviewFormComponent, LeafletMapComponent],
   standalone: true,
   templateUrl: './offer-page.component.html',
   styleUrl: './offer-page.component.css'
