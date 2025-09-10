c427586af66fc1ddb0f585a83637e2706f55d6e3
cooperfirewalk <cooperfirewalk@gmail.com>
2025-09-10 17:25:34 +0300

    добавляет компоненты reviews-list и review-item

---
+++ src/app/pages/offer-page/reviews-list/review-item/review-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-review-item',
+  imports: [],
+  standalone: true,
+  templateUrl: './review-item.component.html',
+  styleUrl: './review-item.component.css'
+})
+export class ReviewItemComponent {
+
+}
